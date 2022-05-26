445717a66a54252a13cee0610648bfa53e35f171
liutydm <dmytro.liutyi@bluejeans.com>
2022-05-26 12:38:55 +0300

    first commit

---
+++ sdkgithubactions/ActionHandler.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace sdkgithubactions
+{
+    public static class ActionHandler
+    {
+        public static string Invoke()
+        {
+            Console.WriteLine("ActionHandler::Invoke is invoked");
+            return JsonConvert.SerializeObject(DateTime.Now);
+        }
+    }
+}
